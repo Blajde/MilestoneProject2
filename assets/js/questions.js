0b6b47bca17cbd23370d68792b70957782d51824
Blajde <blide.daniel@gmail.com>
2021-08-05 18:17:50 +0000

    Add questions.js with questionaire

---
+++ assets/js/questions.js
@@ -0,0 +1,91 @@
+let questionCollection = [
+    {
+        question: "What's the name of the founder of Super Mario?",
+        1: "Shigefumi Hino",
+        2: "Koji Kondo",
+        3: "Shigeru Miyamoto",
+        4: "Saturo Okada"
+        rightAnswer: "3",
+    },
+    {
+        question: "When was the Gameboy launched in Europe?",
+        1: "1990",
+        2: "1992",
+        3: "1989",
+        4: "1991",
+        rightAnswer: "1",
+    },
+    {
+        
+        question "What's the name of the main protagonist in the Legend of Zelda-franchise?",
+        1: "Zelda",
+        2: "Navi",
+        3: "Zora",
+        4: "Link",
+        rightAnswer: "4",
+    },
+    {
+        question: "What's the name of Sonic the Hedgehogs fox friend?",
+        1: "Tails",
+        2: "Knuckles",
+        3: "Rosy",
+        4: "Silver",
+
+        rightAnswer: "1",
+    },
+    {
+        question: "Which game has sold the most copies up to date?",
+        1: "Minecraft",
+        2: "Tetris",
+        3: "Wii Sports",
+        4: "Super Mario bros 3",
+
+        rightAnswer: "1",
+    },
+    {
+        question: "How many Pokémon were there originally?",
+        1: "100",
+        2: "150",
+        3: "125",
+        4: "200",
+
+        rightAnswer: "2",
+    },
+    {
+        question: "Who wrote the main theme for The Legend of Zelda?",
+        1: "Jeremy Soule",
+        2: "Harry Gregson-Wiliams",
+        3: "Koji Kondo",
+        4: "Yoko Shimomura",
+
+        rightAnswer: "3",
+    },
+    {
+        question: "In which Final Fantasy - game is the main characters' name Claude?",
+        1: "X",
+        2: "VII",
+        3: "III",
+        4: "V",
+
+        rightAnswer: "2",
+    },
+    {
+        question: "What was Nintendo's original business?",
+        1: "Cameras",
+        2: "Electronics",
+        3: "Video Games",
+        4: "Playing Cards",
+
+        rightAnswer: "4",
+    },
+    {
+        question: "What kind of animal is Yoshi?",
+        1: "Monkey",
+        2: "Dinosaur",
+        3: "Fox",
+        4: "Raccoon",
+
+        rightAnswer: "2",
+    },
+
+];
