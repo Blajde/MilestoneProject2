075e2ddd330be182e78e8c44e9d565514720308c
Blajde <blide.daniel@gmail.com>
2021-08-05 18:28:08 +0000

    Add function steps in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 
 //Start game//
-
+function startGame()
 //First question//
-
+function checkAnswer()
 //Next question//
-
+function nextQuestion()
 //Game end//
-
+function finishGame()
 //Getting points//
-
+function correctAnswer()
+//Wrong answer//
+function incorrectAnswer()
