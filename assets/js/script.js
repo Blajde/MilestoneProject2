5b3c37dd89aa36d03ea2504c8305a3594731de45
Blajde <blide.daniel@gmail.com>
2021-08-08 19:05:12 +0000

    Add functions for check answer and next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,39 @@
 let question = document.getElementById('myQuestion');
 let alternatives = document.getElementsByClassName('alt');
 
-
-document.addEventListener('DOMContentLoaded', function(){
+//Start game//
+document.addEventListener('DOMContentLoaded', function() {
     question.textContent = gameQuestions[0]['question']
+//-------------------------------------------------------------//
+    alternatives.textContent = gameQuestions[0]['alt']
 
+    for (let i = 0; i < gameQuestions.length; i++)
+        
 
 })
+
+
+//Next question//
+function nextQuestion() {
+    for (i=0; i=gameQuestions.length; i++) {
+        question.textContent = gameQuestions[i++]['question']
+    };
+}
+
+
+//check answer//
+
+function checkAnswer()
+    if (rightAnswer === true) {
+        document.getElementsByClassName('alt'."".fontcolor("#14eb05"))
+    } else {
+        document.getElementsByClassName('alt'."".fontcolor("#f02000"))
+    };
+
+
+//Increment score//
+
+
 
 
 
